324c20738c08dd708ea84646912bb7da7c6f3d9e
AKc3280787 <49967106+AKc3280787@users.noreply.github.com>
2019-04-25 18:18:01 +1000

    Pseduo
    Added a step list to the head of the code.

--- Main.c
+++ Main.c
@@ -1,3 +1,9 @@
+// This is what I have to do
+// step one, open files in and out which the program will assume that they are preexisting
+// step two, read the in file for a rotation key that will range from 1 to 24.
+// step three, complete a while loop while the EOF is greater than 0.
+// step four, store the results in an array until needed.
+// step five, print the array to the out file thus the project is done.
 #include <stdio.h>
 
 int main()
